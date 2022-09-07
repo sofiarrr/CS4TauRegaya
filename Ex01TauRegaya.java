874b590c7562e63de5debdee2126dc9499214fa4
sofiarrr <112992119+sofiarrr@users.noreply.github.com>
2022-09-07 08:58:36 +0800

    Add files via upload

---
+++ Ex01TauRegaya.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex01.tau.regaya;
+
+/**
+ *
+ * @author TAU
+ */
+public class Ex01TauRegaya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Me";
+        
+    }
+    
+}
